24c1689e02615d6c03d862356656bb5977851ebc
Phill Farrugia <me@phillfarrugia.com>
2016-06-05 12:14:37 +1000

    Adds initial xcode project

---
+++ Currency Converter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Currency Converter
+//
+//  Created by Phill Farrugia on 5/06/2016.
+//  Copyright © 2016 Phill Farrugia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Currency ConverterTests/Currency_ConverterTests.swift
@@ -0,0 +1,36 @@
+//
+//  Currency_ConverterTests.swift
+//  Currency ConverterTests
+//
+//  Created by Phill Farrugia on 5/06/2016.
+//  Copyright © 2016 Phill Farrugia. All rights reserved.
+//
+
+import XCTest
+@testable import Currency_Converter
+
+class Currency_ConverterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
