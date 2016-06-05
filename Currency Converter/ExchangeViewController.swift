19a01721195a712f59640488dcf0201329fc1219
Phill Farrugia <me@phillfarrugia.com>
2016-06-05 12:28:41 +1000

    Changes preferred status bar colour to white

--- Currency Converter/ExchangeViewController.swift
+++ Currency Converter/ExchangeViewController.swift
@@ -19,6 +19,10 @@ class ExchangeViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
 
 }
 
