f691f54b860a20f03f86356d8a029574c8656d7f
Phill Farrugia <me@phillfarrugia.com>
2016-06-05 22:50:25 +1000

    Limits text field decimals and decimal points

--- Currency Converter/ExchangeViewController.swift
+++ Currency Converter/ExchangeViewController.swift
@@ -8,14 +8,19 @@
 
 import UIKit
 
-class ExchangeViewController: UIViewController, CurrencySelectorViewDelegate {
+class ExchangeViewController: UIViewController, CurrencySelectorViewDelegate, UITextFieldDelegate {
     
     @IBOutlet weak private var currencySelectorView: CurrencySelectorView!
     @IBOutlet weak var baseCurrencyLabel: UILabel!
-    @IBOutlet weak private var baseAmountTextField: UITextField!
+    @IBOutlet weak private var baseAmountTextField: DynamicWidthTextField!
     
     static private let baseCurrencyName = "AUD"
     static private let conversionCurrencyNames = [ "CAD", "EUR", "GBP", "JPY", "USD" ]
+    
+    static private let defaultTextFieldAttributes: [String: AnyObject] = [
+        NSForegroundColorAttributeName: UIColor.whiteColor(),
+        NSFontAttributeName: UIFont(name: "HelveticaNeue-Medium", size: 44)!
+    ]
     
     private var currencies: [Currency]? {
         didSet {
@@ -42,20 +47,9 @@ class ExchangeViewController: UIViewController, CurrencySelectorViewDelegate {
     }
     
     private func setupSubViews() {
-        
         baseCurrencyLabel.text = ExchangeViewController.baseCurrencyName
-        applyDottedUnderlineToView(baseAmountTextField, color: UIColor(red:0.30, green:0.27, blue:0.29, alpha:1.0))
-    }
-    
-    func applyDottedUnderlineToView(view: UIView, color: UIColor) {
-        let borderLayer = CAShapeLayer()
-        borderLayer.strokeColor = color.CGColor
-        borderLayer.fillColor = nil
-        borderLayer.lineWidth = 6
-        borderLayer.lineDashPattern = [6, 3]
-        borderLayer.path = UIBezierPath(rect: CGRectMake(0, view.bounds.height
-            , view.bounds.width, 3)).CGPath
-        view.layer.addSublayer(borderLayer)
+        baseAmountTextField.delegate = self
+        baseAmountTextField.addTarget(self, action: #selector(ExchangeViewController.textFieldTextDidChange(_:)), forControlEvents: .EditingChanged)
     }
     
     private func requestLatestCurrencyData() {
@@ -86,6 +80,78 @@ class ExchangeViewController: UIViewController, CurrencySelectorViewDelegate {
     func selectorDidSelectItemAtIndex(index: Int) {
         print("Index Selected: \(index)")
     }
+    
+    // MARK: UITextFieldDelegate
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        if let textFieldText = textField.text {
+            let proposedText = (textFieldText as NSString).stringByReplacingCharactersInRange(range, withString: string)
+            
+            if ((proposedText as NSString).rangeOfString(".").location != NSNotFound) {
+                // Restrict number of decimal places (".") characters to 1
+                if (proposedText.componentsSeparatedByString(".").count > 2) {
+                    return false
+                }
+                else if (proposedText.componentsSeparatedByString(".")[1].characters.count > 2) {
+                    // Restrict number of digits after decimal to 2
+                    return false
+                }
+            }
+            
+            // Filter out invalid characters
+            return !containsInvalidCharacters(proposedText)
+        }
+        return !containsInvalidCharacters(string)
+    }
+    
+    internal func textFieldTextDidChange(textField: UITextField) {
+        UIView.animateWithDuration(0.1) {
+            textField.invalidateIntrinsicContentSize()
+        }
+    }
+    
+    private func containsInvalidCharacters(text: String) -> Bool {
+        let range = (text as NSString).rangeOfCharacterFromSet(NSCharacterSet.letterCharacterSet())
+        if (range.location != NSNotFound) {
+            return true
+        }
+        else {
+            return false
+        }
+    }
 
+}
+
+// NB: Subclassing UITextField to override intrinsicContentSize and
+// enable a dynamic width while user is typing
+class DynamicWidthTextField: UITextField {
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        applyDottedUnderlineToView(self, color: UIColor(red:0.30, green:0.27, blue:0.29, alpha:1.0))
+    }
+    
+    func applyDottedUnderlineToView(view: UIView, color: UIColor) {
+        let borderLayer = CAShapeLayer()
+        borderLayer.strokeColor = color.CGColor
+        borderLayer.fillColor = nil
+        borderLayer.lineWidth = 6
+        borderLayer.lineDashPattern = [6, 3]
+        borderLayer.path = UIBezierPath(rect: CGRectMake(0, view.bounds.height
+            , view.bounds.width, 3)).CGPath
+        view.layer.addSublayer(borderLayer)
+    }
+    
+    override func intrinsicContentSize() -> CGSize {
+        if let text = self.text {
+            if (text.characters.count > 0) {
+                let size = (text as NSString).sizeWithAttributes(ExchangeViewController.defaultTextFieldAttributes)
+                return CGSizeMake(size.width, self.frame.height)
+            }
+        }
+        return super.intrinsicContentSize()
+    }
+    
 }
 
