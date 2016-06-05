007187cedae6b142de3889a9122db266a9652d70
Phill Farrugia <me@phillfarrugia.com>
2016-06-05 12:40:25 +1000

    Adds clear currency selector view

--- Currency Converter/ExchangeViewController.swift
+++ Currency Converter/ExchangeViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ExchangeViewController: UIViewController {
-
+    
+    @IBOutlet weak private var currencySelectorView: HorizontalLabelSelectorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
---
+++ Currency Converter/HorizontalLabelSelectorView.swift
@@ -0,0 +1,13 @@
+//
+//  HorizontalLabelSelectorView.swift
+//  Currency Converter
+//
+//  Created by Phill Farrugia on 5/06/2016.
+//  Copyright © 2016 Phill Farrugia. All rights reserved.
+//
+
+import UIKit
+
+class HorizontalLabelSelectorView: UIView {
+
+}
